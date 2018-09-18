6e411351803ebc2de645af207a6259758941095a
451yuhua <451174313@qq.com>
2018-09-18 22:01:35 +0800

    新增swagger调试

--- src/main/java/com/bootMybatis/controller/UserController.java
+++ src/main/java/com/bootMybatis/controller/UserController.java
@@ -1,21 +1,43 @@
 package com.bootMybatis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bootMybatis.pojo.User;
 import com.bootMybatis.service.UserService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(tags="swaggerDemoController相关的api")
 public class UserController {
 
 	@Autowired
 	private UserService userInterface;
 	
-	@RequestMapping("getUser")
-	public User getUser(Integer id) {
+	@RequestMapping("getUser/{id}")
+	@ApiOperation(value = "根据id查询用户信息", notes = "查询数据库中某个用户的信息")
+	@ApiImplicitParam(name = "id", value = "用户id",paramType="path", required = true, dataType = "Integer")
+	public User getUser(@PathVariable Integer id) {
 		return userInterface.findUserById(id);
+	}
+	
+	@ApiOperation(value="test", notes = "test...")
+	@GetMapping("/test")
+	public String testAPI() {
+		return "test";
+	}
+	
+	@GetMapping("/getStr")
+	@ApiOperation(value = "根据id查询用户信息", notes = "查询数据库中某个用户的信息")
+	@ApiImplicitParam(name = "str", value = "str",paramType="query", required = true, dataType = "String")
+	public String getStr(String str) {
+		return str;
 	}
 	
 }
